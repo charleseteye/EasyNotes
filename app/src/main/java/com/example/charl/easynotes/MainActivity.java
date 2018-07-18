9f1d594637d05e11cd072a14e712f4156bd57200
CHARLES <charlesdev48@gmail.com>
2018-07-18 11:40:28 +0300

    addition of more compeonents

--- app/src/main/java/com/example/charl/easynotes/MainActivity.java
+++ app/src/main/java/com/example/charl/easynotes/MainActivity.java
@@ -7,9 +7,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.charl.easynotes.model.NoteEntity;
+import com.example.charl.easynotes.ui.NotesAdapter;
+import com.example.charl.easynotes.utilities.SampleData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.recycler_view)
     RecyclerView mRecyclerView;
+    private NotesAdapter mAdapter;
+
+    private List<NoteEntity> notesData=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +49,21 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        notesData.addAll(SampleData.getNotes());
+        for (NoteEntity note:
+             notesData) {
+            Log.i("PlainoldNotes", note.toString());
+            
+        }
     }
 
     private void initRecyclerview() {
         mRecyclerView.setHasFixedSize(true);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(linearLayoutManager);
+
+        mAdapter=new NotesAdapter(notesData,this);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     @Override
